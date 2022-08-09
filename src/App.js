5ec30f6886b0b43ab5bf291e205d5feb209b7071
MUHAMMADASIF123 <52148492+MUHAMMADASIF123@users.noreply.github.com>
2022-08-09 21:58:28 +0500

    update the app.css
    "this is happend sometime"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Active from "./Components/Active";
+import Navbar from "./Components/Navbar";
+import Archive from './Components/Archive'
+import Draft from './Components/Draft'
+import {Routes, Route} from 'react-router-dom'
+import Addusers from "./Components/Addusers";
+import Editeuser from "./Components/Editeuser";
+import View from "./Components/View";
+import SideNavbar from "./Components/SideNavbar";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Navbar/>
+        
+      <Routes>
+     
+        <Route path="/" element={<Active/>}/>
+        <Route path="archive" element={<Archive/>}/>
+        <Route path="draft" element={<Draft/>}/>
+        <Route path="users/add" element={<Addusers/>}/>
+        <Route path="/users/edit/:id" element={<Editeuser/>}/>
+        <Route path="/users/:id" element={<View/>}/>
+        </Routes>
+    <Footer/>
     </div>
   );
 }
